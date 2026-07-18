f596c05d519de11352cf5179cdddb22d74587620
Turja1111 <saumik.das.turja@gmail.com1>
2026-07-18 21:15:02 +0600

    update

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --no-input
-python manage.py load_routine --path routine_extracted.json --semester fall --year 2026
